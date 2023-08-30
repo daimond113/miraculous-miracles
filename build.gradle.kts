5e9de255d82ecf307903539e9fed1c36ae09e537
dai <72147841+daimond113@users.noreply.github.com>
2023-08-30 12:38:58 +0200

    feat: in-game documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,11 @@ repositories {
 	// Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
 	// See https://docs.gradle.org/current/userguide/declaring_repositories.html
 	// for more information about repositories.
+
+	maven {
+		name = "Patchouli"
+		url = uri("https://maven.blamejared.com")
+	}
 }
 
 // All the dependencies are declared at gradle/libs.version.toml and referenced with "libs.<id>"
@@ -53,6 +58,8 @@ dependencies {
 	// modImplementation(libs.bundles.qfapi) // If you wish to use the deprecated Fabric API modules
 
 	modImplementation(libs.qkl)
+
+	modImplementation(libs.patchouli)
 }
 
 tasks {
